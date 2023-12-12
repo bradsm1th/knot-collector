69045d2e07b96971b466d35e759288ff2c11f046
Brad Smith <bradsmith@duck.com>
2023-12-11 20:13:55 -0500

    Add knot model and update its view functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from .models import Knot
+# knots = [
+#   {
+#     'name': 'bowline', 
+#     'types': ('loop', 'basic'), 'description': 'Makes a loop in the end of a rope'
+#     },
+#   {
+#     'name': 'figure eight', 
+#     'types': ('stopper',), 'description': 'A good stopper knot'
+#     },
+# ]
+
 # Create your views here.
+def about(request):
+  return render(request, 'about.html')
+
+
+def home(request):
+  return render(request, 'home.html')
+
+
+def knots_index(request):
+  knots = Knot.objects.all()
+  return render(request, 'knots/index.html', {'knots': knots})
