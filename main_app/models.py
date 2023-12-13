0d811ff1255ae6e95517de66f747901cdab38bb8
Brad Smith <bradsmith@duck.com>
2023-12-13 14:47:27 -0500

    Add 'type' model, rename it to be clearer

--- main_app/models.py
+++ main_app/models.py
@@ -2,20 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
-class Knot(models.Model):
-  name = models.CharField(max_length=100)
-  # type = models.CharField(max_length=50)
-  description = models.CharField(max_length=250)
-
-
-  def __str__(self):
-    return self.name
-  
-
-  def get_absolute_url(self):
-    return reverse('detail', kwargs={'knot_id': self.id})
-
-
 # data for knot types model
 KNOT_TYPES = (
  ('b', 'bend'),
@@ -25,8 +11,31 @@ KNOT_TYPES = (
  ('s', 'stopper'),
 )
 
-class Type(models.Model):
+class TypeOfKnot(models.Model):
   type = models.CharField(
     max_length=1,
-    choices=KNOT_TYPES
+    choices=KNOT_TYPES,
+    default=KNOT_TYPES[3][0],
     )
+
+  def __str__(self):
+    return f"{self.get_type_display()}"
+  
+
+class Knot(models.Model):
+  name = models.CharField(max_length=100)
+  # type = models.CharField(max_length=50)
+  description = models.CharField(max_length=250)
+  type = models.ForeignKey(
+    TypeOfKnot,
+    on_delete=models.CASCADE,
+    )
+
+
+  def __str__(self):
+    return self.name
+  
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'knot_id': self.id})
+
